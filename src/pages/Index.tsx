ba91923c04c7e4234447d6f9326efcc27c81ce79
Joshua Osorio <joshua.osorio@uabc.edu.mx>
2025-09-13 09:36:56 -0700

    cargar icono de la pagina"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -227,7 +227,7 @@ const Index = () => {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
               <div className="flex items-center space-x-2 mb-4">
-                <img src="/logos/logo_2.ico" alt="Baja Film Factory" className="w-8 h-8" />
+                <img src="/logos/factory.ico" alt="Baja Film Factory" className="w-8 h-8" />
                 <span className="text-lg font-bold">Baja Film Factory</span>
               </div>
               <p className="text-sm text-muted-foreground">
